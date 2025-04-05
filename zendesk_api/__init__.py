ec5e334b969c18d2bbcdb87c7258cf6cc31f70ab
VScristianlazar <cristian.lazar@vetstrategy.com>
2025-04-05 11:32:29 -0400

    Create zendesk_api package

---
+++ zendesk_api/__init__.py
@@ -0,0 +1,8 @@
+"""
+Zendesk API Integration Package.
+
+This package provides modules for interacting with the Zendesk API,
+including authentication, ticket management, and monitoring.
+"""
+
+__version__ = "1.0.0"
